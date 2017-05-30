298df544157de4582cb5a112e9f44c2de3331e61
tatarrr <tatarrr95@gmail.com>
2017-05-30 22:05:08 +1000

    3 day

--- app/js/main.js
+++ app/js/main.js
@@ -9,9 +9,10 @@ $(document).ready(function(){
 
   $(".complete-objects-slider").owlCarousel({
   	loop:true,
-    margin:10,
+    margin:15,
     nav:true,
-    items:12,
+    items:4,
+    rewind: true,
     dots: false
   });
   $(".bigSlider-slider").owlCarousel({
@@ -20,3 +21,44 @@ $(document).ready(function(){
     items: 1
   });
 });
+
+/*Карта*/
+
+    // Создает обработчик события window.onLoad
+    ymaps.ready(function() {
+        // Создает экземпляр карты и привязывает его к созданному контейнеру
+        var map = new ymaps.Map("YMapsID", {
+                center: [43.101423, 131.944870],
+                zoom: 16
+                    //controls: ['zoomControl']
+            }),
+            traffic = new ymaps.control.TrafficControl();
+
+        map.controls.add(traffic);
+        // traffic.showTraffic();
+
+        map.behaviors.disable('scrollZoom');
+        map.controls.remove('zoomControl');
+
+        placemark = new ymaps.Placemark(
+            [43.101734, 131.948654], { iconContent: "", iconContentSize: [300, 100] }, { preset: 'islands#blueCircleIcon' }
+        );
+
+        var coords = [43.101735, 131.949185],
+            placemark1 = new ymaps.GeoObjectCollection({}, {
+                iconLayout: 'default#image',
+                iconImageClipRect: [
+                    [0, 0],
+                    [33, 42]
+                ],
+                iconImageHref: 'img/map-marker1.png',
+                iconImageSize: [33, 42],
+                iconImageOffset: [-33, -42]
+            });
+        placemark1.add(new ymaps.Placemark(coords));
+
+
+        map.geoObjects.add(placemark1);
+    });
+
+// ................................................... /////////
